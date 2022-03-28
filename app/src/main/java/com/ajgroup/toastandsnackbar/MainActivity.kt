9400de9a2049b23e31e6e966e67f0c31ad61d7f0
aldi7id <aldi7.indo@gmail.com>
2022-03-28 21:30:33 +0700

    add toast and snacbkbar with action

--- app/src/main/java/com/ajgroup/toastandsnackbar/MainActivity.kt
+++ app/src/main/java/com/ajgroup/toastandsnackbar/MainActivity.kt
@@ -2,7 +2,9 @@ package com.ajgroup.toastandsnackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ajgroup.toastandsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnToast.setOnClickListener {
+            Toast.makeText(this, "Ini Toast", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("OKE") { }
+                .show()
+        }
     }
 }
