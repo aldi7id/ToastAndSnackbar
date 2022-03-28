409b8fe8f8ca4c646de2e289ee9064991355fb33
aldi7id <aldi7.indo@gmail.com>
2022-03-28 21:15:29 +0700

    add & setting viewbinding and layout setting

--- app/src/main/java/com/ajgroup/toastandsnackbar/MainActivity.kt
+++ app/src/main/java/com/ajgroup/toastandsnackbar/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ajgroup.toastandsnackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ajgroup.toastandsnackbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
